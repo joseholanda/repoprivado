b24ff3bdcb66e58775003ab8058c9e6f7a6eab59
José Holanda Neto <jose.holanda.neto@gmail.com>
2020-10-27 14:38:54 -0300

    first comit

---
+++ src/main/java/br/com/tecnisys/repoprivado/RepoprivadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.repoprivado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoprivadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoprivadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tecnisys/repoprivado/RepoprivadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tecnisys.repoprivado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoprivadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
